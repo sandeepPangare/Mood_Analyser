0a76a797c08ec382694cbfd0bebf780ae0f85547
sandeepPangare <pangaresandeep96@gmail.com>
2022-12-05 22:23:21 +0530

    Added:Analyse and respond happy or sad mood

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
+
     @Test
     public void givenMessage_WhenProper_ShouldReturnSad(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in sad mood");
+        String mood = moodAnalyser.analyseMood("This is a sad message");
         System.out.println(mood);
         Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a happy message");
+        System.out.println(mood);
+        Assertions.assertEquals("HAPPY", mood);
     }
 }
